63a047d7ed1ede80ad1b8765f8e9f32f9fb7e046
dineshbaddawar <dineshbaddawar@gmail.com>
2023-12-13 09:21:51 +0530

    13/12/2023 commit

--- force-app/main/default/lwc/folderItem/folderItem.js
+++ force-app/main/default/lwc/folderItem/folderItem.js
@@ -1,3 +1,64 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, api, wire, track } from 'lwc';
+import folderpng from '@salesforce/resourceUrl/folderpng';
+import PreviewPDF from '@salesforce/apex/FileExplorerHelper.PreviewPDF';
 
-export default class FolderItem extends LightningElement {}
+
+export default class FolderItem extends LightningElement {
+     @api item = [];
+     selectedRecordIdPDF;
+     showPDF = false;
+     error;
+     pdfUrl;
+     folderfile = folderpng;
+
+     handleItemClickNew(event){
+          debugger;
+          this.selectedRecordIdPDF = event.currentTarget.dataset.recordId;
+          this.getViewPreviewFile();
+          console.log('Selected Record ID:', selectedRecordId);
+     }
+     
+     getViewPreviewFile(){
+          debugger;
+          PreviewPDF({procDocId : this.selectedRecordIdPDF})
+          .then(result =>{
+              if(result){
+              this.showPDF = true;
+               if(result){
+                   // Create a Blob from base64-encoded string
+                  const pdfBlob = this.base64ToBlob(result, 'application/pdf');
+                  // Generate a Blob URL for the PDF
+                  this.pdfUrl = URL.createObjectURL(pdfBlob);
+               }
+              }
+          })
+          .catch(error =>{
+           this.error = error;
+          })
+     }
+     
+    base64ToBlob(base64String, contentType) {
+     debugger;
+     const byteCharacters = atob(base64String);
+     const byteArrays = [];
+     for (let offset = 0; offset < byteCharacters.length; offset += 512) {
+         const slice = byteCharacters.slice(offset, offset + 512);
+         const byteNumbers = new Array(slice.length);
+         for (let i = 0; i < slice.length; i++) {
+             byteNumbers[i] = slice.charCodeAt(i);
+         }
+         const byteArray = new Uint8Array(byteNumbers);
+         byteArrays.push(byteArray);
+     }
+         return new Blob(byteArrays, { type: contentType });
+    }
+     
+     deleteFiles() {
+          debugger;
+     }
+     
+     uploadFile() {
+          debugger;
+     }
+     
+}
